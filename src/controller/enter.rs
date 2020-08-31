b14bc525ae08327745aea3780297c3d52dfe8a95
Leopold Luley <git@leopoldluley.de>
2020-08-31 13:05:03 +0200

    Update Druid to Git version.

--- src/controller/enter.rs
+++ src/controller/enter.rs
@@ -1,4 +1,4 @@
-use druid::{widget::Controller, Env, Event, EventCtx, KeyCode, Widget};
+use druid::{keyboard_types::Key, widget::Controller, Env, Event, EventCtx, Widget};
 use std::any::Any;
 
 pub struct EnterController<D> {
@@ -16,8 +16,8 @@ impl<D> EnterController<D> {
 impl<D, W: Widget<D>> Controller<D, W> for EnterController<D> {
     fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut D, env: &Env) {
         child.event(ctx, event, data, env);
-        if let Event::KeyUp(key) = event {
-            if key.key_code == KeyCode::Return {
+        if let Event::KeyUp(event) = event {
+            if event.key == Key::Enter {
                 (self.callback)(ctx, data);
             }
         }
